112e7465f67102186c29542045bdd31f715d8e8f
amitshah995 <amitshah995@gmail.com>
2026-09-11 12:38:00 +0530

    Update google-home.tsx

--- components/layout/google-home.tsx
+++ components/layout/google-home.tsx
@@ -19,8 +19,7 @@ export function GoogleSearchHome() {
   const searchRef = useRef<HTMLDivElement>(null)
   const searchInputRef = useRef<HTMLInputElement>(null)
 
-  const instagramAuthUrl = `https://www.instagram.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_INSTAGRAM_APP_ID}&redirect_uri=${process.env.NEXT_PUBLIC_INSTAGRAM_REDIRECT_URI}&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish%2Cinstagram_business_manage_insights#weblink`
-
+  const instagramAuthUrl = `https://www.instagram.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_INSTAGRAM_APP_ID}&redirect_uri=${process.env.NEXT_PUBLIC_INSTAGRAM_REDIRECT_URI}&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments#weblink`
   // Animated rotating search placeholders
   const placeholders = [
     "Try: 'price' to auto-send catalog in DMs...",
